1bb1c416f477c7554a90b443b5f4bdafcb62e74f
Sammy Baek <whitesw01@gmail.com>
2023-08-08 01:33:16 +0900

    Sync styles of InNav with OutNav's

--- src/navigation/InNav.tsx
+++ src/navigation/InNav.tsx
@@ -3,14 +3,32 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // Screens
 import Home from '~/screens/Home/Home';
 
+// Styles
+import { useTheme } from 'styled-components/native';
+
 // Types
 import type { InNavStackParamList } from '~/types/react-navigation';
 
 const Nav = createNativeStackNavigator<InNavStackParamList>();
 
 const InNav = () => {
+  const theme = useTheme();
+
   return (
-    <Nav.Navigator>
+    <Nav.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.navHeaderBgColor,
+        },
+        headerTintColor: theme.navHeaderTintColor,
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          color: theme.navHeaderTextColor,
+          fontWeight: 'normal',
+        },
+        animation: 'slide_from_right',
+      }}
+    >
       <Nav.Screen name="Home" component={Home} />
     </Nav.Navigator>
   );
